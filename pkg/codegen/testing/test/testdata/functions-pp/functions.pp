93cf6fc898112fcf4293aaa4a9510848c99c8489
aq17 <null>
2022-10-17 17:18:40 +0000

    🔄 Synced local 'pkg/codegen/testing/test/testdata' with remote 'pkg/pulumiyaml/testing/test/testdata/'

--- pkg/codegen/testing/test/testdata/functions-pp/functions.pp
+++ pkg/codegen/testing/test/testdata/functions-pp/functions.pp
@@ -1,13 +1,3 @@
 encoded = toBase64("haha business")
 
-decoded = fromBase64(encoded)
-
-joined = join("-", [encoded, decoded, "2"])
-
-resource bucket "aws:s3:Bucket" {
-
-}
-
-encoded2 = toBase64(bucket.id)
-
-decoded2 = fromBase64(bucket.id)
+joined = join("-", ["haha", "business"])
